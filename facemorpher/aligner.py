59fd64e6f1f5b9313631f4c3a38554b638c8ae62
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2020-09-01 21:18:45 +0900

    Update aligner.py

--- facemorpher/aligner.py
+++ facemorpher/aligner.py
@@ -4,7 +4,7 @@ Align face and image sizes
 import cv2
 import numpy as np
 
-def positive_cap(num):
+def PositiveCap(num):
   """ Cap a number to ensure positivity
 
   :param num: positive or negative number
@@ -15,28 +15,28 @@ def positive_cap(num):
   else:
     return num, 0
 
-def roi_coordinates(rect, size, scale):
+def RoiCoordinates(boundingRectangle, desiredSize, scalingFactor):
   """ Align the rectangle into the center and return the top-left coordinates
   within the new size. If rect is smaller, we add borders.
 
-  :param rect: (x, y, w, h) bounding rectangle of the face
-  :param size: (width, height) are the desired dimensions
-  :param scale: scaling factor of the rectangle to be resized
+  :param boundingRectangle: (x, y, w, h) bounding rectangle of the face
+  :param desiredSize: (width, height) are the desired dimensions
+  :param scalingFactor: scaling factor of the rectangle to be resized
   :returns: 4 numbers. Top-left coordinates of the aligned ROI.
     (x, y, border_x, border_y). All values are > 0.
   """
-  rectx, recty, rectw, recth = rect
-  new_height, new_width = size
-  mid_x = int((rectx + rectw/2) * scale)
-  mid_y = int((recty + recth/2) * scale)
-  roi_x = mid_x - int(new_width/2)
-  roi_y = mid_y - int(new_height/2)
+  rectangle_x, rectangle_y, rectangle_w, rectangle_h = boundingRectangle
+  desiredHeight, desiredWidth = desiredSize
+  mid_x = int((rectangle_x + rectangle_w / 2) * scalingFactor)
+  mid_y = int((rectangle_y + rectangle_h / 2) * scalingFactor)
+  roi_x = mid_x - int(desiredWidth / 2)
+  roi_y = mid_y - int(desiredHeight / 2)
 
-  roi_x, border_x = positive_cap(roi_x)
-  roi_y, border_y = positive_cap(roi_y)
+  roi_x, border_x = PositiveCap(roi_x)
+  roi_y, border_y = PositiveCap(roi_y)
   return roi_x, roi_y, border_x, border_y
 
-def scaling_factor(rect, size):
+def CalculateScalingFactor(boundingRectangle, desiredSize):
   """ Calculate the scaling factor for the current image to be
       resized to the new dimensions
 
@@ -44,32 +44,32 @@ def scaling_factor(rect, size):
   :param size: (width, height) are the desired dimensions
   :returns: floating point scaling factor
   """
-  new_height, new_width = size
-  rect_h, rect_w = rect[2:]
-  height_ratio = rect_h / new_height
-  width_ratio = rect_w / new_width
-  scale = 1
-  if height_ratio > width_ratio:
-    new_recth = 0.8 * new_height
-    scale = new_recth / rect_h
+  desiredHeight, desiredWidth = desiredSize
+  rectangle_w, rectangle_h = boundingRectangle[2:]
+  heightRatio = rectangle_h / desiredHeight
+  widthRatio = rectangle_w / desiredWidth
+  scalingFactor = 1
+  if heightRatio > widthRatio:
+    newRectangle_h = 0.95 * desiredHeight
+    scalingFactor = newRectangle_h / rectangle_h
   else:
-    new_rectw = 0.8 * new_width
-    scale = new_rectw / rect_w
-  return scale
+    newRectangle_w = 0.95 * desiredWidth
+    scalingFactor = newRectangle_w / rectangle_w
+  return scalingFactor
 
-def resize_image(img, scale):
+def ResizeImage(sourceImage, scalingFactor):
   """ Resize image with the provided scaling factor
 
   :param img: image to be resized
-  :param scale: scaling factor for resizing the image
+  :param scalingFactor: scaling factor for resizing the image
   """
-  cur_height, cur_width = img.shape[:2]
-  new_scaled_height = int(scale * cur_height)
-  new_scaled_width = int(scale * cur_width)
+  currentHeight, currnetWidth = sourceImage.shape[:2]
+  newScaledHeight = int(scalingFactor * currentHeight)
+  newScaledWidth = int(scalingFactor * currnetWidth)
 
-  return cv2.resize(img, (new_scaled_width, new_scaled_height))
+  return cv2.resize(sourceImage, (newScaledWidth, newScaledHeight), interpolation = cv2.INTER_LANCZOS4)
 
-def resize_align(img, points, size):
+def ResizeAlign(sourceImage, points, desiredSize):
   """ Resize image and associated points, align face to the center
     and crop to the desired size
 
@@ -77,26 +77,31 @@ def resize_align(img, points, size):
   :param points: *m* x 2 array of points
   :param size: (height, width) tuple of new desired size
   """
-  new_height, new_width = size
+  desiredHeight, desiredWidth = desiredSize
 
   # Resize image based on bounding rectangle
-  rect = cv2.boundingRect(np.array([points], np.int32))
-  scale = scaling_factor(rect, size)
-  img = resize_image(img, scale)
+  boundingRectangle = cv2.boundingRect(np.array([points], np.int32))
+  scalingFactor = CalculateScalingFactor(boundingRectangle, desiredSize)
+  resizedImage = ResizeImage(sourceImage, scalingFactor)
 
   # Align bounding rect to center
-  cur_height, cur_width = img.shape[:2]
-  roi_x, roi_y, border_x, border_y = roi_coordinates(rect, size, scale)
-  roi_h = np.min([new_height-border_y, cur_height-roi_y])
-  roi_w = np.min([new_width-border_x, cur_width-roi_x])
+  currentHeight, currnetWidth = resizedImage.shape[:2]
+  roi_x, roi_y, border_x, border_y = RoiCoordinates(boundingRectangle, desiredSize, scalingFactor)
+  roi_h = np.min([desiredHeight, desiredHeight - border_y, currentHeight, currentHeight - roi_y])
+  roi_w = np.min([desiredWidth, desiredWidth - border_x, currnetWidth, currnetWidth - roi_x])
 
   # Crop to supplied size
-  crop = np.zeros((new_height, new_width, 3), img.dtype)
-  crop[border_y:border_y+roi_h, border_x:border_x+roi_w] = (
-     img[roi_y:roi_y+roi_h, roi_x:roi_x+roi_w])
+  crop = resizedImage[roi_y:roi_y + roi_h, roi_x:roi_x + roi_w]
+  crop = cv2.normalize(crop, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX)
+
+  paddingTop = border_y
+  paddingBottom = desiredHeight - roi_h - border_y
+  paddingLeft = border_x
+  paddingRight = desiredWidth - roi_w - border_x
+  crop = cv2.copyMakeBorder(crop, paddingTop, paddingBottom, paddingLeft, paddingRight, cv2.BORDER_REPLICATE)
 
   # Scale and align face points to the crop
-  points[:, 0] = (points[:, 0] * scale) + (border_x - roi_x)
-  points[:, 1] = (points[:, 1] * scale) + (border_y - roi_y)
+  points[:, 0] = (points[:, 0] * scalingFactor) + (border_x - roi_x)
+  points[:, 1] = (points[:, 1] * scalingFactor) + (border_y - roi_y)
 
   return (crop, points)
