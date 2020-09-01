1fa7afa3dc3df4530c9931f0028e6094f403b30c
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2020-09-01 21:19:12 +0900

    Update averager.py

--- facemorpher/averager.py
+++ facemorpher/averager.py
@@ -18,7 +18,7 @@
     --out=<filename>       Filename to save the average face [default: result.png]
     --destimg=<filename>   Destination face image to overlay average face
     --plot                 Flag to display the average face [default: False]
-    --background=<bg>      Background of image to be one of (black|transparent|average) [default: black]
+    --background=<bg>      Background of image to be one of (black|transparent|average) [default: average]
     --version              Show version.
 """
 
@@ -26,93 +26,127 @@ from docopt import docopt
 import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
-from facemorpher import locator
-from facemorpher import aligner
-from facemorpher import warper
-from facemorpher import blender
-from facemorpher import plotter
+import locator
+import aligner
+import warper
+import blender
+import plotter
 
-def list_imgpaths(imgfolder):
-  for fname in os.listdir(imgfolder):
+def ListImagePaths(imageFolder):
+  for fname in os.listdir(imageFolder):
     if (fname.lower().endswith('.jpg') or
        fname.lower().endswith('.png') or
        fname.lower().endswith('.jpeg')):
-      yield os.path.join(imgfolder, fname)
+      yield os.path.join(imageFolder, fname)
 
-def sharpen(img):
-  blured = cv2.GaussianBlur(img, (0, 0), 2.5)
-  return cv2.addWeighted(img, 1.4, blured, -0.4, 0)
+def UnsharpMasking(sourceImage, radius = (5, 5), sd = 2.5):
+  """
+  :param sourceImage: float32 BGR image.
+  :param radius: taple(n, m): radius of Gaussian blur. it is must be odd number.
+  :param sd: standard deviation.
+  :returns: float32 BGR image.
+  """
+  blured = cv2.GaussianBlur(sourceImage, radius, sd)
+  return cv2.addWeighted(sourceImage, 2.00, blured, -1.00, 0)
 
-def load_image_points(path, size):
-  img = cv2.imread(path)
-  points = locator.face_points(img)
+def AdjustTone(sourceImage):
+  """
+  :param sourceImage: float32 or float64 BGR image.
+  :returns: float32 or float64 BGR image(range: 0.0 to 255.0).
+  """
+  adjustedImage = (sourceImage.max() - sourceImage.min()) * (-2.0 * cv2.pow(((sourceImage - sourceImage.min()) / (sourceImage.max() - sourceImage.min())), 3)
+    + 3.0 * cv2.pow(((sourceImage - sourceImage.min()) / (sourceImage.max() - sourceImage.min())), 2))
+  adjustedImage = cv2.normalize(adjustedImage, 0.0, 255.0, norm_type=cv2.NORM_MINMAX)
+  adjustedImage = cv2.addWeighted(sourceImage, 0.9, adjustedImage, 0.1, 0)
+  adjustedImage = adjustedImage.clip(0.0, 255.0)
+  return adjustedImage
+
+def loadImagePoints(path, desiredSize, background):
+  sourceImage = cv2.imread(path)
+  points = locator.face_points(sourceImage, background)
 
   if len(points) == 0:
     print('No face in %s' % path)
     return None, None
   else:
-    return aligner.resize_align(img, points, size)
+    return aligner.ResizeAlign(sourceImage, points, desiredSize)
 
-def averager(imgpaths, dest_filename=None, width=500, height=600, background='black',
-             blur_edges=False, out_filename='result.png', plot=False):
+def averager(imagePaths, destinationFilename = None, width = 500, height = 600, background = 'average',
+             blurEdges = False, outputFilename = 'result.png', plot = False):
 
   size = (height, width)
-
   images = []
-  point_set = []
-  for path in imgpaths:
-    img, points = load_image_points(path, size)
-    if img is not None:
-      images.append(img)
-      point_set.append(points)
+  pointSet = []
+  counter = 0
+  print("Now preparating images...")
+  for path in imagePaths:
+    sourceImage, points = loadImagePoints(path, size, background)
+    if sourceImage is not None:
+      images.append(sourceImage)
+      pointSet.append(points)
+    counter += 1
+    print(counter)
 
   if len(images) == 0:
-    raise FileNotFoundError('Could not find any valid images.' +
-                            ' Supported formats are .jpg, .png, .jpeg')
+    raise FileNotFoundError('Could not find any valid images.' + ' Supported formats are .jpg, .png, .webp')
 
-  if dest_filename is not None:
-    dest_img, dest_points = load_image_points(dest_filename, size)
-    if dest_img is None or dest_points is None:
-      raise Exception('No face or detected face points in dest img: ' + dest_filename)
+  if destinationFilename is not None:
+    destinationImage, destinationPoints = loadImagePoints(destinationFilename, size, background)
+    if destinationImage is None or destinationPoints is None:
+      raise Exception('No face or detected face points in dest img: ' + destinationFilename)
   else:
-    dest_img = np.zeros(images[0].shape, np.uint8)
-    dest_points = locator.average_points(point_set)
+    destinationImage = np.zeros(images[0].shape, np.uint8)
+    destinationPoints = locator.average_points(pointSet)
 
-  num_images = len(images)
-  result_images = np.zeros(images[0].shape, np.float32)
-  for i in range(num_images):
-    result_images += warper.warp_image(images[i], point_set[i],
-                                       dest_points, size, np.float32)
+  print("Now averaging and mixing images...")
+  print("Maybe it takes a few minutes...")
+  nImages = len(images)
+  resultImage = np.zeros(images[0].shape, np.float32)
+  counter = 0
+  for i in range(0, nImages):
+    resultImage += warper.warp_image(images[i], pointSet[i], destinationPoints, size, np.float64)
+    counter += 1
+    print(counter)
 
-  result_image = np.uint8(result_images / num_images)
-  face_indexes = np.nonzero(result_image)
-  dest_img[face_indexes] = result_image[face_indexes]
+  resultImage = resultImage / nImages
+  resultImage = (resultImage - np.mean(resultImage)) / np.std(resultImage) * 72 + 72
+  minimumSize = min(size)
+  unsharpRadius = int(minimumSize / 128)
+  if unsharpRadius % 2 == 0:
+    unsharpRadius += 1
+  if unsharpRadius < 3:
+    unsharpRadius = 3
+  resultImage = UnsharpMasking(resultImage, radius = (unsharpRadius, unsharpRadius), sd = 2.5)
+  resultImage = UnsharpMasking(resultImage, radius = (unsharpRadius, unsharpRadius), sd = 2.5)
+  resultImage = AdjustTone(resultImage)
 
-  mask = blender.mask_from_points(size, dest_points)
-  if blur_edges:
-    blur_radius = 10
-    mask = cv2.blur(mask, (blur_radius, blur_radius))
+  facePixelIndexes = np.nonzero(resultImage)
+  destinationImage[facePixelIndexes] = resultImage[facePixelIndexes]
+
+  mask = blender.mask_from_points(size, destinationPoints)
+  mask = mask.astype(np.float64)
+  if blurEdges:
+    blurKernelSize = 9
+    mask = cv2.blur(mask, (blurKernelSize, blurKernelSize))
 
   if background in ('transparent', 'average'):
-    dest_img = np.dstack((dest_img, mask))
+    destinationImage = np.dstack((destinationImage, mask))
 
     if background == 'average':
-      average_background = locator.average_points(images)
-      dest_img = blender.overlay_image(dest_img, mask, average_background)
+      averageBackground = locator.average_points(images)
+      destinationImage = blender.overlay_image(destinationImage, mask, averageBackground)
 
-  print('Averaged {} images'.format(num_images))
-  plt = plotter.Plotter(plot, num_images=1, out_filename=out_filename)
-  plt.save(dest_img)
-  plt.plot_one(dest_img)
-  plt.show()
+  destinationImage = cv2.normalize(destinationImage, 0.0, 65535.0, norm_type=cv2.NORM_MINMAX)
+  destinationImage = destinationImage.astype(np.uint16)
+  print('Averaged {} images'.format(nImages))
+  #cv2.imwrite("./mask.png", mask)
+  cv2.imwrite(outputFilename, destinationImage)
 
 def main():
   args = docopt(__doc__, version='Face Averager 1.0')
   try:
-    averager(list_imgpaths(args['--images']), args['--destimg'],
+    averager(ListImagePaths(args['--images']), args['--destimg'],
              int(args['--width']), int(args['--height']),
              args['--background'], args['--blur'], args['--out'], args['--plot'])
   except Exception as e:
